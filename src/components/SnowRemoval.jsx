3e20eb09cfc3bd092ab845a5bda0800a4caddc36
Aftab Patel <pate2003@mylaurier.ca>
2026-02-08 00:52:48 +0000

    commit everything

--- src/components/SnowRemoval.jsx
+++ src/components/SnowRemoval.jsx
@@ -1,7 +1,7 @@
 import { snowremoval } from "../snowremoval.js";
 import { Text } from "react-native";
 // input cm of snowfall
-export function SnowRemoval(cm){
+export function SnowRemoval({cm}){
     return(
         <text>
             Snow removal of {cm} requires {snowremoval(cm)}
